015ff9a0d1b4354c9149b7699509750ef1ba4dc3
Thomas Melo <thomas.melo@terra.com.br>
2023-09-11 22:15:53 -0300

    Lancamento: término do formulário

--- resources/views/lancamento/form.blade.php
+++ resources/views/lancamento/form.blade.php
@@ -39,19 +39,46 @@
         <label for="id_centro_custo" class="form-label">Centro de Custo*</label>
         <select id="id_centro_custo" class="form-select" required>
             <option value="">Escolha...</option>
-            <option value="">...</option>
+                @foreach ($centrosDeCusto::orderBy('centro_custo')->get() as $centro )
+                    <option value="{{$centro->id_centro_custo}}"
+                        @selected(
+                            (
+                                $lancamento &&
+                                $lancamento->id_centro_custo == $centro->id_centro_custo
+                            )
+                            ||
+                            old('id_centro_custo') == $centro->id_centro_custo
+                        )
+                    >
+                        {{ $centro->centro_custo}}
+                    </option>
+                @endforeach
         </select>
     </div>
     <div class="col-md-4">
         <label class="form-label" for="valor">Valor*</label>
-        <input class="form-control" type="number" id="valor" name="valor" value="" required>
+        <input class="form-control" type="number" id="valor" name="valor"
+         value="{{
+            $lancamento ?
+            $lancamento->valor :
+            old('valor')
+          }}"
+         required>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-12">
         <label class="form-label" for="descricao">Descrição*</label>
-        <input class="form-control" type="text" id="descricao" name="descricao" value="" required>
+        <input class="form-control" type="text" id="descricao" name="descricao"
+        value="{{
+            $lancamento ?
+            $lancamento->descricao :
+            old('descricao')
+          }}"
+          required>
     </div>
     <div class="col-md-2 offset-md-9">
-        <input class="btn btn-primary" type="submit" value=" Cadastrar  Atualizar">
+        <input class="btn btn-primary" type="submit"
+         value="{{ $lancamento ? 'Atualizar' : 'Cadastrar' }}"
+         >
     </div>
 
 
